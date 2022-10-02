26fbc479a45ce8932767b8ffcd7ad933eb09aafd
duncan-ship-it <duncanelliot2@gmail.com>
2022-10-02 21:09:56 +1100

    mostly finished layout

--- app/src/main/java/com/example/trading_app/MainActivity.kt
+++ app/src/main/java/com/example/trading_app/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.trading_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.trading_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+
+        binding.portfolioButton.setOnClickListener {
+            startActivity(Intent(this, PortfolioActivity::class.java))
+        }
+
+        binding.tradesButton.setOnClickListener {
+            startActivity(Intent(this, StocksActivity::class.java))
+        }
+
+        setContentView(binding.root)
     }
 }
